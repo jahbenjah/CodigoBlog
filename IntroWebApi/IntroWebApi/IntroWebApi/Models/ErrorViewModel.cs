3b541e6b90bafe56c8f225980620ff1c329714ce
Benjamin Camacho <jahbenjah@hotmail.com>
2020-02-07 23:51:31 -0600

    Version inicial

---
+++ IntroWebApi/IntroWebApi/IntroWebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IntroWebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
