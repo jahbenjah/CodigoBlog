66a5c7e679c772365f5607a7212f02db555a079a
Benjamin Camacho <jahbenjah@hotmail.com>
2020-03-14 18:38:53 -0600

    Se agrega el proyecto para articulo del Select

---
+++ ElementoSelect/ElementoSelect/ElementoSelect/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ElementoSelect.Models;
+
+namespace ElementoSelect.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new Pedido();
+            model.Clientes = new List<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem>()
+            {
+                new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem("Seleccionar cliente","0",true),
+                new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem("Cliente 2","1"),
+                new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem("Cliente 2","2"),
+                new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem("Cliente 3","3"),
+                new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem("Cliente 4","4"),
+            };
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ElementoSelect/ElementoSelect/ElementoSelect/wwwroot/js/site.js
@@ -0,0 +1,83 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+const selectElement = document.getElementById('Id');
+selectElement.addEventListener('change', (event) => {
+    var select = event.target;
+    var select = event.target.s
+    alert(`You like ${select.value} : ${select.options.textContent}`);
+});
+
+
+
+////Select OdbcDriver Event
+//$('#DriverId').on('change', function () {
+//    var driverId = $(this).val();
+//    if (driverId === 14 || driverId === 15) {
+//        btnTest.data("validate", "file");
+//        btnTest.data("type", "file");
+//    } else {
+//        btnTest.data("validate", "odbc");
+//        btnTest.data("type", "odbc");
+//    }
+//    console.log(btnTest.data("validate"));
+//    $.ajax({
+//        type: "GET",
+//        url: '/Odbc/driverfields/' + driverId,
+//        success: function (data) {
+//            $.each(data, function (k, v) {
+//                let slctor = '#' + k;
+//                v ? $(slctor).removeClass('d-none') : $(slctor).addClass('d-none');
+//            });
+//            ajaxCallGetFiles(driverId);
+//        },
+//        dataType: "JSON"
+//    });
+
+//});
+
+
+//$(function () {
+//    //Select OdbcDriver Event
+//    $('#Id').on('change', function () {
+//        var driverId = $(this).val();
+//        $.ajax({
+//            type: "GET",
+//            url: '/Odbc/driverfields/' + driverId,
+//            success: function (data) {
+//                $.each(data, function (k, v) {
+//                    let slctor = '#' + k;
+//                    v ? $(slctor).removeClass('d-none') : $(slctor).addClass('d-none');
+//                });
+//                ajaxCallGetFiles(driverId);
+//            },
+//            dataType: "JSON"
+//        });
+//    });
+
+//    function ajaxCallGetFiles(driverId) {
+//        let select = $('#FilePath');
+//        select.empty();
+//        if (driverId !== '0') {
+//            $.ajax({
+//                type: 'GET',
+//                url: '/Odbc/ShowFiles/' + filter,
+//                success: function (data) {
+//                    $.each(data, function (key, value) {
+//                        select.append(
+//                            $('<option>', {
+//                                value: value.value,
+//                                text: value.text,
+//                                selected: value.selected,
+//                                disabled: value.disabled
+//                            })
+//                        );
+//                    });
+//                }
+//            });
+//        }
+//    }
+//});
